bc2201eb862c83e6cf112077c7ebfa3ec36cc784
viktorpr14 <viktor.zhybak@gmail.com>
2014-12-03 19:53:19 +0200

    Replace HibernateDAO to package repository

--- src/main/java/org/softserveinc/service/UserService.java
+++ src/main/java/org/softserveinc/service/UserService.java
@@ -1,6 +1,7 @@
 package org.softserveinc.service;
 
 import org.softserveinc.domain.User;
+import org.softserveinc.repository.HibernateDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 

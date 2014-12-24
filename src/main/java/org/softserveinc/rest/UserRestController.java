7db26e0929296a6bcf1d4b34f2b095368e7e8026
Oleh Kushniryk <kushnirykoleh@gmail.com>
2014-12-24 17:50:42 +0200

    add basic support of spring 4 rest service

--- src/main/java/org/softserveinc/rest/UserRestController.java
+++ src/main/java/org/softserveinc/rest/UserRestController.java
@@ -2,25 +2,19 @@ package org.softserveinc.rest;
 
 import com.google.gson.Gson;
 import org.softserveinc.service.UserService;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
 
-
-@Path("/userProfile/{username}")
-@Service
-public class UserRestService {
+@RestController
+@RequestMapping("/rest/userProfile")
+public class UserRestController {
     @Inject
     private UserService userService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getUser(@PathParam("username") String userName) {
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public String getUser(@PathVariable("username") String userName) {
         if(SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser")){
             return"Please login";
         }

491e116d664d6cf051b201b0415060ab698e1523
viktorpr14 <viktor.zhybak@gmail.com>
2015-01-21 19:42:03 +0200

    Improved tree structure of bookmarks on user profile

--- src/main/webapp/resources/directives/directives.js
+++ src/main/webapp/resources/directives/directives.js
@@ -2,6 +2,7 @@
  * Created by vv on 18.01.2015.
  */
 
+/*
 app.directive('collection', function() {
     return {
         restrict: "E",
@@ -12,7 +13,9 @@ app.directive('collection', function() {
         template: "<ul><member ng-repeat='member in collection' member='member'></member></ul>"
     }
 });
+*/
 
+/*
 app.directive('member', function($compile) {
     return {
         restrict: "E",
@@ -32,6 +35,49 @@ app.directive('member', function($compile) {
                     "</ul>");
                 $compile(element.contents())(scope)
             }
+        }
+    }
+});*/
+
+app.directive('collection', function() {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            collection: '='
+        },
+        template: "<member ng-repeat='member in collection' member='member'></member>"
+    }
+});
+
+app.directive('member', function($compile) {
+    return {
+        restrict: "E",
+        replaced: true,
+        scope: {
+            member: '='
+        },
+        template:
+        "<li class='folderName' style='list-style: none'>{{member.folderName}}</li>",
+
+        link: function(scope, element, attrs) {
+
+            var nestedFolders = "<ul><collection collection='member.listOfTreeNodes'></collection></ul>";
+
+            var nestedBookmarks =
+                "<ul>" +
+                    "<li class='bookmarkName' ng-repeat='bookmark in member.listOfBookmarks' style='list-style: none'>" +
+                        "<a ng-if='bookmark.name'  href='{{bookmark.URL}}' target='_blank'>{{bookmark.name}}</a>" +
+                        "<a ng-if='!bookmark.name' href='{{bookmark.URL}}' target='_blank'>{{bookmark.URL}} </a>" +
+                    "</li>" +
+                "</ul>";
+
+            if (angular.isArray(scope.member.listOfTreeNodes)) {
+                element.append(nestedFolders + nestedBookmarks);
+            } else {
+                element.append(nestedBookmarks);
+            }
+            $compile(element.contents())(scope)
         }
     }
 });

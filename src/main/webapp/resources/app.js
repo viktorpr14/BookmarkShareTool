626cb9f33f302e9708dbf7e50abc180475524dec
viktorpr14 <viktor.zhybak@gmail.com>
2015-01-26 22:29:59 +0200

    Added ability to open and close folders with bookmarks on user profile

--- src/main/webapp/resources/app.js
+++ src/main/webapp/resources/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('userApp', ['ngRoute', 'ngAnimate', 'angularTreeview']);
+var app = angular.module('userApp', ['ngRoute', 'ngAnimate']);
 
 
 app.config(function($routeProvider) {
--- src/main/webapp/resources/directives/directives.js
+++ src/main/webapp/resources/directives/directives.js
@@ -57,27 +57,33 @@ app.directive('member', function($compile) {
         scope: {
             member: '='
         },
-        template:
-        "<li class='folderName' style='list-style: none'>{{member.folderName}}</li>",
+        template: "",
 
         link: function(scope, element, attrs) {
 
-            var nestedFolders = "<ul><collection collection='member.listOfTreeNodes'></collection></ul>";
+            var pre = "<li class='closedFolder' style='list-style: none' ng-init='member.isHidden=false'>" +
+                         "<a href ng-click='member.isHidden=!member.isHidden' style='text-decoration: none; color: black'>{{member.folderName}}</a>" +
+                      "</li>";
+
+            var nestedFolders = "<ul ng-hide='member.isHidden'><collection collection='member.listOfTreeNodes'></collection></ul>";
 
             var nestedBookmarks =
-                "<ul>" +
-                    "<li class='bookmarkName' ng-repeat='bookmark in member.listOfBookmarks' style='list-style: none'>" +
+                "<ul ng-hide='member.isHidden'>" +
+                    "<li class='bookmark' ng-repeat='bookmark in member.listOfBookmarks' style='list-style: none'>" +
                         "<a ng-if='bookmark.name'  href='{{bookmark.URL}}' target='_blank'>{{bookmark.name}}</a>" +
                         "<a ng-if='!bookmark.name' href='{{bookmark.URL}}' target='_blank'>{{bookmark.URL}} </a>" +
                     "</li>" +
                 "</ul>";
 
+            var result;
+
             if (angular.isArray(scope.member.listOfTreeNodes)) {
-                element.append(nestedFolders + nestedBookmarks);
+                element.append(pre + nestedFolders + nestedBookmarks);
             } else {
-                element.append(nestedBookmarks);
+                element.append(pre + nestedBookmarks);
             }
             $compile(element.contents())(scope)
+
         }
     }
 });

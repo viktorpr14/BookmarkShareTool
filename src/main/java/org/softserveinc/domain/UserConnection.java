0323c527f7b1680b09db305c53a61d53cbabbb6b
viktorpr14 <viktor.zhybak@gmail.com>
2015-02-06 16:42:49 +0200

    Added table for social provider connection

---
+++ src/main/java/org/softserveinc/domain/UserConnection.java
@@ -0,0 +1,95 @@
+package org.softserveinc.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.math.BigInteger;
+
+/**
+ * Created by vv on 03.02.2015.
+ */
+@Entity
+public class UserConnection {
+
+    @EmbeddedId
+    private UserConnectionId userConnectionId;
+    private Integer rank;
+    private String displayName;
+    private String profileUrl;
+    private String imageUrl;
+    private String accessToken;
+    private String secret;
+    private String refreshToken;
+    private BigInteger expireTime;
+
+    public UserConnectionId getUserConnectionId() {
+        return userConnectionId;
+    }
+
+    public void setUserConnectionId(UserConnectionId userConnectionId) {
+        this.userConnectionId = userConnectionId;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public BigInteger getExpireTime() {
+        return expireTime;
+    }
+
+    public void setExpireTime(BigInteger expireTime) {
+        this.expireTime = expireTime;
+    }
+}
---
+++ src/main/java/org/softserveinc/domain/UserConnectionId.java
@@ -0,0 +1,38 @@
+package org.softserveinc.domain;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+/**
+ * Created by vv on 04.02.2015.
+ */
+@Embeddable
+public class UserConnectionId implements Serializable{
+    private String userId;
+    private String providerId;
+    private String providerUserId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getProviderUserId() {
+        return providerUserId;
+    }
+
+    public void setProviderUserId(String providerUserId) {
+        this.providerUserId = providerUserId;
+    }
+}
